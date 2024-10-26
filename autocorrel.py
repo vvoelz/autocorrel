1a93dc8d59c509d34dded941b39780cda746b2b8
Vincent Voelz <voelz@temple.edu>
2024-10-26 03:40:09 -0400

    added normalization

--- autocorrel.py
+++ autocorrel.py
@@ -6,6 +6,8 @@ import numpy as np
 def autocorr(x):
     tau = x.size
     mu = x.mean()
-    g = np.correlate(x, x, mode='full')[tau-1:]
+    var = x.var()  # Variance for normalization
+    g = np.correlate((x-mu), (x-mu), mode='full')[tau-1:]
+    g = g / var
     n = np.arange(tau,0,-1)
     return g/n
